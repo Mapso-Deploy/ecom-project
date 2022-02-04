fcd61a92c0efc422f0557e5ff2bee08273edcd4a
kvin-stphns <kevin@mapso.co>
2022-02-04 14:40:32 -0500

    updating blog styles

--- src/components/Iframe.js
+++ src/components/Iframe.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Iframe = () => {
   return (
-<iframe title="Mapso Blog" src="https://cascadr.co/blogs/cozykev" width="100%" height="100%" position="relative" allowFullScreen></iframe>
+<iframe title="Mapso Blog" src="https://cascadr.co/blogs/cozykev" width="100%" height="100%" position="relative" overflow= "hidden" allowFullScreen></iframe>
 )
 }
 
