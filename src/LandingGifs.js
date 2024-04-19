ed0707fae60d3cd142b4f00e80e0d632ea26e503
mapso-deploy <kevin@mapso.co>
2024-04-19 20:27:36 +0100

    Optimizing landing
    deleted unused gifs, about to modify the landing page to use MOV over gifs.

---
+++ src/LandingGifs.js
@@ -0,0 +1,24 @@
+// import React from 'react'
+// import Mapso from "./MapsoEnergy4.gif"
+// import logo from "./mapso.gif"
+// import { Link } from "react-router-dom"
+// import './styles.css';
+// import ReactFreezeframe from "react-freezeframe";
+
+
+// export default function landing() {
+//     return (
+//         <div className="Logo" style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '100vh', overflow: 'hidden'}}>
+//         {/* <a href="/home"> <img src={logo} style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '10vh', onLoad: 'fadeIn'}} /> </a> */}
+//             <div className="animated-gif-box"> 
+//             <Link to="/Products">
+//                 <ReactFreezeframe src={Mapso} className="animated-gif"/>
+//             </Link>
+//             </div>
+//             <div className="mobile-logo-box">
+//             <a href="/Products"> <img src={logo} alt="mobile logo" className="mobile-logo" /> </a>
+//             </div>
+//         </div> 
+//     )
+// }
+
