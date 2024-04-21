9a7ea25bbcebadfbf47c816f8de0a4b7d53d1040
mapso-deploy <kevin@mapso.co>
2024-04-21 20:57:52 +0100

    reverting url

--- src/components/ModelsMapped.js
+++ src/components/ModelsMapped.js
@@ -54,7 +54,7 @@ export default function Model(props) {
                     data-item-id={product.id}
                     data-item-price={product.price}
                     data-item-description={product.description}
-                    data-item-url={`https://www.mapso.co/products/${product.id}`}
+                    data-item-url={`/products/${product.id}`}
                     // data-item-image={product.image}
                     data-item-name={product.name}
                     data-item-custom1-name={product.custom1Name}
